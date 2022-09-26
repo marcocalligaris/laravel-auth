bd10fb333393092488b9ce0791e3fd2d7089c38d
marcocalligaris <marcocally@gmail.com>
2022-09-26 19:17:32 +0200

    Guest route added

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
-
 Auth::routes();
 
 Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
 
     // Route::resource('posts', 'PostController');
+    
+    Route::get('/{any}', function () {
+        abort('404');
+    })->where('any', '.*');
 });
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
