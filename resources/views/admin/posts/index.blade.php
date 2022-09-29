02b7151b7eb5db5b6b1706c59e6fba5218cf1860
marcocalligaris <marcocally@gmail.com>
2022-09-29 16:11:46 +0200

    Added category column

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -14,6 +14,7 @@
             <th scope="col">#</th>
             <th>Titolo</th>
             <th>Slug</th>
+            <th>Categoria</th>
             <th>Creato il</th>
             <th>Modificato il</th>
             <th>Azioni</th>
@@ -25,27 +26,28 @@
                 <th scope="row">{{ $post->id }}</th>
                 <td>{{ $post->title }}</td>
                 <td>{{ $post->slug }}</td>
+                <td> @if ($post->category)  {{ $post->category->label }} @else  Generic @endif </td>
                 <td>{{ $post->created_at }}</td>
                 <td>{{ $post->updated_at }}</td>
                 <td class="d-flex justify-content-end">
-                  <a class="btn btn-sm btn-info mr-2 text-white" href="{{ route('admin.posts.edit', $post) }}">
-                    <i class="fa-solid fa-pencil mr-2"></i>Modifica
+                  <a class="btn btn-sm btn-info text-white mr-2" href="{{ route('admin.posts.edit', $post) }}">
+                    <i class="fa-solid fa-pencil"></i>
                   </a>
                   <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST" class="delete-form">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-danger btn-sm mr-2" type="submit">
-                        <i class="fa-solid fa-trash mr-2"></i>Elimina
+                        <i class="fa-solid fa-trash"></i>
                     </button>
                 </form>
                   <a class="btn btn-sm btn-primary" href="{{ route('admin.posts.show', $post) }}">
-                    <i class="fa-solid fa-eye mr-2"></i>Vedi
+                    <i class="fa-solid fa-eye"></i>
                   </a>
                 </td>
             </tr>
             @empty
             <tr>
-                <td colspan="6">
+                <td colspan="7">
                     <h3 class="text-center">Nessun post</h3>
                 </td>
             </tr>
