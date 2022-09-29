2f857e133bd71f6f804586dca63bbefd31682acf
marcocalligaris <marcocally@gmail.com>
2022-09-29 15:08:33 +0200

    Fillable category id

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'image', 'content', 'slug'];
+    protected $fillable = ['title', 'image', 'content', 'slug', 'category_id'];
 
     public function category()
     {
