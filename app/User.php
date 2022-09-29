d29a4739ba9f4003107142c13eb8e9c9113fcb50
marcocalligaris <marcocally@gmail.com>
2022-09-29 18:07:27 +0200

    Setted relationships

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Models\Post');
     }
+
+    public function userInfo()
+    {
+        return $this->hasOne('App\Models\UserInfo');
+    }
 }
